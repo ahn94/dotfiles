b56fe6951d94fa9d7ecbb24e54e73ca229d093d8
Austin Nightingale <austnig94@gmail.com>
2018-01-22 20:58:27 -0600

    Add .conky config

---
+++ conky/.conky/conky-startup.sh
@@ -0,0 +1,6 @@
+sleep 20s
+killall conky
+cd "/home/ahn94/.conky/TeejeeTech"
+conky -c "/home/ahn94/.conky/TeejeeTech/Network Panel" &
+cd "/home/ahn94/.conky/TeejeeTech"
+conky -c "/home/ahn94/.conky/TeejeeTech/Process Panel" &
